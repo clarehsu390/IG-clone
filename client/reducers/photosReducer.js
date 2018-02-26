86b1aea61028d332addcfcf5429a5276c15b0b31
Clare Hsu <clare.hsu390@gmail.com>
2018-02-25 19:16:34 -0800

    create photo reducer

--- client/reducers/photosReducer.js
+++ client/reducers/photosReducer.js
@@ -1,7 +1,18 @@
+import { RECEIVE_PHOTO, RECEIVE_ALL_PHOTOS } from '../actions/PhotoActions';
+
 const photosReducer = (
-    state = [], action
-) => {
-    return state;
+    state = [], action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_ALL_PHOTOS:
+        const photos = action.photos;
+        return photos;
+        case RECEIVE_PHOTO:
+            return action.photo;
+        default:
+            return state;
+
+    }
 }
 
 export default photosReducer;
